054ae50fd6f9f9d3a22235176f1384147bc8ad62
Juliano Ramos <juliano.ramos@bplus.com.br>
2022-08-09 17:38:47 -0300

    feat: formulários

--- App.tsx
+++ App.tsx
@@ -10,8 +10,11 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
 import AppLoading from 'expo-app-loading';
+
+import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +29,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
